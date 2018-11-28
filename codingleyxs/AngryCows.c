780742cd82c45a2be7275e3dc2328f05fb51f60a
Javecc <1420755674@qq.com>
2018-11-28 17:47:21 +0800

    add some files

---
+++ codingleyxs/AngryCows.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#define MAXLEN 101
+int simulate()   //模拟爆炸
+{
+
+}
+
+int main(void)
+{
+    int n,i,j;
+    int location[MAXLEN];
+    int now;       //当前的坐标点
+    int maxnum = 1;    //最大爆炸数，初始化为1
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(j=0;j<n;j++){
+
+    } 
+    return 0;
+}
--- codingleyxs/MovingTheField.c
+++ codingleyxs/MovingTheField.c
@@ -1,1 +1,15 @@
 #include<stdio.h>
+#define MAXI 100
+#define MAXJ 100
+#define LENGTH 101
+int simulate(){
+
+}
+int main(void){
+    int n;
+    char
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+
+    }
+}
